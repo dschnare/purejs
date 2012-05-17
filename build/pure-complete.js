b697297b557e80217a724ed547ce18a66ba20484
Darren Schnare <darren@darrenschnare.com>
2012-05-16 22:08:16 -0400

    Refactor how purejs properties are exported

--- build/pure-complete.js
+++ build/pure-complete.js
@@ -211,158 +211,159 @@
 
 			return o;
 		},
-		pure = {};
+		pure = {
+			"isString": isString,
+			"isBoolean": isBoolean,
+			"isNumber": isNumber,
+			"isFunction": isFunction,
+			"isArray": isArray,
+			"isObject": isObject,
+			"isDefined": isDefined,
+			"isUndefined": isUndefined,
+			"typeOf": typeOf,
+			"mixin": mixin,
+			"adheresTo": function (o, interfce) {
+				var key,
+					typeofo,
+					typeofi;
 
-	xport('isString', isString, pure);
-	xport('isBoolean', isBoolean, pure);
-	xport('isNumber', isNumber, pure);
-	xport('isFunction', isFunction, pure);
-	xport('isArray', isArray, pure);
-	xport('isObject', isObject, pure);
-	xport('isDefined', isDefined, pure);
-	xport('isUndefined', isUndefined, pure);
-	xport('typeOf', typeOf, pure);
-	xport('mixin', mixin, pure);
-	xport('adheresTo', function (o, interfce) {
-		var key,
-			typeofo,
-			typeofi;
-
-		if ((isObject(o) || isFunction(o) || isArray(o)) &&
-				(isObject(interfce) || isFunction(interfce) || isArray(interfce))) {
-			for (key in interfce) {
-				if (interfce.hasOwnProperty(key)) {
-					// Property can be any type, but must exist.
-					if (interfce[key] === '*') {
-						if (o[key] === undefined) {
-							return false;
-						}
-					} else {
-						if (typeOf(o[key]) !== typeOf(interfce[key]) &&
-								typeOf(o[key]) !== interfce[key]) {
-							return false;
+				if ((isObject(o) || isFunction(o) || isArray(o)) &&
+						(isObject(interfce) || isFunction(interfce) || isArray(interfce))) {
+					for (key in interfce) {
+						if (interfce.hasOwnProperty(key)) {
+							// Property can be any type, but must exist.
+							if (interfce[key] === '*') {
+								if (o[key] === undefined) {
+									return false;
+								}
+							} else {
+								if (typeOf(o[key]) !== typeOf(interfce[key]) &&
+										typeOf(o[key]) !== interfce[key]) {
+									return false;
+								}
+							}
 						}
 					}
+
+					return true;
 				}
+
+				typeofo = typeOf(o);
+				typeofi = typeOf(interfce);
+
+				return typeofo === typeofi;
+			},
+			"constructor": {
+				"create": (function () {
+					var create = (function () {
+							var F = function () {};
+							return function (o) {
+								F.prototype = o;
+								return new F();
+							};
+						}());
+
+					return function (base, members, name) {
+						var prototype,
+							ctr,
+							createInstance,
+							b = base,
+							m = members,
+							n = name;
+
+						if (arguments.length === 0) {
+							m = {};
+							b = {};
+						} else if (arguments.length === 1) {
+							if (typeof b === 'string') {
+								n = b;
+								m = {};
+								b = {};
+							} else {
+								m = b;
+								b = {};
+							}
+						} else if (arguments.length === 2) {
+							if (isString(m)) {
+								n = m;
+								m = b;
+								b = {};
+							}
+						} else if (arguments.length === 3) {
+							n = isDefined(n) ? n.toString() : '';
+						}
+
+						// Set a default name if none was specified.
+						n = isString(n) && n.length ? n : 'UnnamedConstructor';
+
+						// If base is a function then we assume it's a constructor
+						// so we read its prototype property.
+						//
+						// NOTE: This is allowed due to many programmer's familiarity
+						// with classical models and how classical inheritence is
+						// facilitated. However, this is not the preferred approach.
+						if (isFunction(b)) {
+							b = b.prototype;
+						}
+
+						// Manage our prototype chain by extending the base
+						// prototype if we can, otherwise we mixin the properties from base.
+						try {
+							prototype = create(b);
+						} catch (error) {
+							if (isFunction(b.constructor) && isObject(b.constructor.prototype)) {
+								prototype = create(b.constructor.prototype);
+							} else {
+								prototype = mixin({}, b);
+							}
+						}
+
+						ctr = function (cpy) {
+							var o = create(ctr.prototype);
+
+							// Trigger the copy contructor if we received a single
+							// argument that is an instance of our constructor and
+							// the copy() method exists. Then return the object.
+							if (arguments.length === 1 &&
+									cpy instanceof ctr &&
+									typeof o.copy === 'function') {
+								o.copy(cpy);
+
+							// If we have an init() method then we call it with
+							// the arguments we received from the constructor.
+							} else if (typeof o.init === 'function') {
+								if (arguments.length) {
+									o.init.apply(o, arguments);
+								} else {
+									o.init();
+								}
+							}
+
+							return o;
+						};
+						// Saveguard our constructor code from inspection and
+						// use the constructor name.
+						//ctr.toString = function () {
+						//	return 'function ' + n + ' () { [native code] }';
+						//};
+						// Easy accessor to retrieve the constructor name.
+						// (typically used for reflection purposes)
+						ctr.getName = function () {
+							return n;
+						};
+
+						// Mixin all properties from members onto our prototype.
+						mixin(prototype, m);
+
+						// Setup references to our prototype and constructor.
+						ctr.prototype = prototype;
+						prototype.constructor = ctr;
+
+						return ctr;
+					};
+				}())
 			}
-
-			return true;
-		}
-
-		typeofo = typeOf(o);
-		typeofi = typeOf(interfce);
-
-		return typeofo === typeofi;
-	}, pure);
-	xport('constructor', {}, pure);
-	xport('constructor.create', (function () {
-		var create = (function () {
-				var F = function () {};
-				return function (o) {
-					F.prototype = o;
-					return new F();
-				};
-			}());
-
-		return function (base, members, name) {
-			var prototype,
-				ctr,
-				createInstance,
-				b = base,
-				m = members,
-				n = name;
-
-			if (arguments.length === 0) {
-				m = {};
-				b = {};
-			} else if (arguments.length === 1) {
-				if (typeof b === 'string') {
-					n = b;
-					m = {};
-					b = {};
-				} else {
-					m = b;
-					b = {};
-				}
-			} else if (arguments.length === 2) {
-				if (isString(m)) {
-					n = m;
-					m = b;
-					b = {};
-				}
-			} else if (arguments.length === 3) {
-				n = isDefined(n) ? n.toString() : '';
-			}
-
-			// Set a default name if none was specified.
-			n = isString(n) && n.length ? n : 'UnnamedConstructor';
-
-			// If base is a function then we assume it's a constructor
-			// so we read its prototype property.
-			//
-			// NOTE: This is allowed due to many programmer's familiarity
-			// with classical models and how classical inheritence is
-			// facilitated. However, this is not the preferred approach.
-			if (isFunction(b)) {
-				b = b.prototype;
-			}
-
-			// Manage our prototype chain by extending the base
-			// prototype if we can, otherwise we mixin the properties from base.
-			try {
-				prototype = create(b);
-			} catch (error) {
-				if (isFunction(b.constructor) && isObject(b.constructor.prototype)) {
-					prototype = create(b.constructor.prototype);
-				} else {
-					prototype = mixin({}, b);
-				}
-			}
-
-			ctr = function (cpy) {
-				var o = create(ctr.prototype);
-
-				// Trigger the copy contructor if we received a single
-				// argument that is an instance of our constructor and
-				// the copy() method exists. Then return the object.
-				if (arguments.length === 1 &&
-						cpy instanceof ctr &&
-						typeof o.copy === 'function') {
-					o.copy(cpy);
-
-				// If we have an init() method then we call it with
-				// the arguments we received from the constructor.
-				} else if (typeof o.init === 'function') {
-					if (arguments.length) {
-						o.init.apply(o, arguments);
-					} else {
-						o.init();
-					}
-				}
-
-				return o;
-			};
-			// Saveguard our constructor code from inspection and
-			// use the constructor name.
-			//ctr.toString = function () {
-			//	return 'function ' + n + ' () { [native code] }';
-			//};
-			// Easy accessor to retrieve the constructor name.
-			// (typically used for reflection purposes)
-			ctr.getName = function () {
-				return n;
-			};
-
-			// Mixin all properties from members onto our prototype.
-			mixin(prototype, m);
-
-			// Setup references to our prototype and constructor.
-			ctr.prototype = prototype;
-			prototype.constructor = ctr;
-
-			return ctr;
 		};
-	}()), pure);
 
 	xport.module(pure, function () {
 		xport('PURE', pure);
--- web/inc/scripts/pure-spec.js
+++ web/inc/scripts/pure-spec.js
@@ -1,5 +1,15 @@
+/*global 'define', 'exports', 'window', 'xport'*/
+/*global 'PURE', 'test', 'asyncTest', 'expect', 'module'*/
+/*global 'start', 'stop', 'ok', 'equal', 'notEqual', 'deepEqual'*/
+/*global 'notDeepEqual', 'strictEqual', 'notStrictEqual', 'raises', 'QUnit'*/
+
 (function () {
 	'use strict';
+
+	var Str = String,
+		Num = Number,
+		Bool = Boolean,
+		Arr = Array;
 	module('Adherence Tests');
 
 	test('wild card adheresTo test', function () {
@@ -209,7 +219,7 @@
 		notStrictEqual(o.age, state.a.age, 'Don\'t expect o.age to be equal to a.age.');
 	});
 
-	test ('overwrite property test', function () {
+	test('overwrite property test', function () {
 		var state = setup(), o;
 
 		state.b.age = 3500;
@@ -238,24 +248,19 @@
 	});
 	module('Type Testing Tests');
 
-	var Str = String,
-				Num = Number,
-				Bool = Boolean,
-				Arr = Array;
-
 	test('isString test', function () {
 		ok(PURE.isString('some string literal'), 'Expect string literal to be a string.');
-		ok(PURE.isString(new String('some string literal')), 'Expect string object to be a string.');
+		ok(PURE.isString(new Str('some string literal')), 'Expect string object to be a string.');
 		equal(PURE.isString(4), false, 'Don\'t expect number literal to be a string.');
-		equal(PURE.isString(new Number(4)), false, 'Don\'t expect number object to be a string.');
+		equal(PURE.isString(new Num(4)), false, 'Don\'t expect number object to be a string.');
 		equal(PURE.isString([1, 2, 3]), false, 'Don\'t expect array literal to be a string.');
 	});
 
 	test('isBoolean test', function () {
 		ok(PURE.isBoolean(true), 'Expect boolean literal to be a boolean.');
-		ok(PURE.isBoolean(new Boolean(false)), 'Expect boolean object to be a boolean.');
+		ok(PURE.isBoolean(new Bool(false)), 'Expect boolean object to be a boolean.');
 		equal(PURE.isBoolean(4), false, 'Don\'t expect number literal to be a boolean.');
-		equal(PURE.isBoolean(new Number(4)), false, 'Don\'t expect number object to be a boolean.');
+		equal(PURE.isBoolean(new Num(4)), false, 'Don\'t expect number object to be a boolean.');
 		equal(PURE.isBoolean([1, 2, 3]), false, 'Don\'t expect array literal to be a boolean.');
 		equal(PURE.isBoolean('true'), false, 'Don\'t expect string literal to be a boolean.');
 	});
@@ -264,10 +269,10 @@
 		ok(PURE.isNumber(0), 'Expect number literal integer to be a number.');
 		ok(PURE.isNumber(12.23), 'Expect number literal float to be a number.');
 		ok(PURE.isNumber(12e4), 'Ex[ect number literal exponent to be a number.');
-		ok(PURE.isNumber(new Number(12.34)), 'Expect number object to be a number.');
+		ok(PURE.isNumber(new Num(12.34)), 'Expect number object to be a number.');
 		equal(PURE.isNumber([1, 2, 3]), false, 'Don\'t expect array literal to be a number.');
 		equal(PURE.isNumber('some string literal'), false, 'Don\'t expect string literal to be a number.');
-		equal(PURE.isNumber(new String('some string literal')), false, 'Don\'t expect string object to be a number.');
+		equal(PURE.isNumber(new Str('some string literal')), false, 'Don\'t expect string object to be a number.');
 	});
 
 	test('isFunction test', function () {
@@ -292,12 +297,12 @@
 		extendedArrayObject = new A();
 
 		ok(PURE.isArray([1, 2, 3]), 'Expect array literal to be an array.');
-		ok(PURE.isArray(new Array(3)), 'Expect new Array() object to be an array.');
+		ok(PURE.isArray(new Arr(3)), 'Expect new Array() object to be an array.');
 		ok(PURE.isArray(arrayLikeObject), 'Expect array-like object to be an array.');
 		ok(PURE.isArray(extendedArrayObject), 'Expect extended array object to be an array.');
 		equal(PURE.isArray(33.33), false, 'Don\'t expect number literal to be an array.');
 		equal(PURE.isArray('some string literal'), false, 'Don\'t expect string literal to be an array.');
-		equal(PURE.isArray(new String('some string literal')), false, 'Don\'t expect string object to be an array.');
+		equal(PURE.isArray(new Str('some string literal')), false, 'Don\'t expect string object to be an array.');
 	});
 
 	test('isObject test', function () {
@@ -314,11 +319,11 @@
 		extendedArrayObject = new A();
 
 		ok(PURE.isObject({key: 'value'}), 'Expect object literal to be an object.');
-		ok(PURE.isObject(new Number(4)), 'Expect number object to be an object.');
-		ok(PURE.isObject(new String('4')), 'Expect string object to be an object.');
-		ok(PURE.isObject(new Boolean(false)), 'Expect boolean object to be an object.');
+		ok(PURE.isObject(new Num(4)), 'Expect number object to be an object.');
+		ok(PURE.isObject(new Str('4')), 'Expect string object to be an object.');
+		ok(PURE.isObject(new Bool(false)), 'Expect boolean object to be an object.');
 		ok(PURE.isObject([1, 2, 3]), 'Expect array literal to be an object.');
-		ok(PURE.isObject(new Array(3)), 'Expect new Array() object to be an object.');
+		ok(PURE.isObject(new Arr(3)), 'Expect new Array() object to be an object.');
 		ok(PURE.isObject(extendedArrayObject), 'Expect extended array object to be an object.');
 		ok(PURE.isObject(arrayLikeObject), 'Expect array-like object to be an object.');
 		equal(PURE.isObject(1), false, 'Don\'t expect number literal to be an object.');
@@ -347,9 +352,9 @@
 		strictEqual(PURE.typeOf('string'), 'string', 'Expect string literal to be typeof "string".');
 		strictEqual(PURE.typeOf(true), 'boolean', 'Expect boolean literal to be typeof "boolean".');
 		strictEqual(PURE.typeOf(function () {}), 'function', 'Expect function literal to be typeof "function".');
-		strictEqual(PURE.typeOf(new Number(34)), 'object', 'Expect number object to be typeof "object".');
-		strictEqual(PURE.typeOf(new String('string')), 'object', 'Expect string object to be typeof "object".');
-		strictEqual(PURE.typeOf(new Boolean(true)), 'object', 'Expect boolean object to be typeof "object".');
+		strictEqual(PURE.typeOf(new Num(34)), 'object', 'Expect number object to be typeof "object".');
+		strictEqual(PURE.typeOf(new Str('string')), 'object', 'Expect string object to be typeof "object".');
+		strictEqual(PURE.typeOf(new Bool(true)), 'object', 'Expect boolean object to be typeof "object".');
 		strictEqual(PURE.typeOf(null), 'null', 'Expect null to be typeof "null".');
 		strictEqual(PURE.typeOf(undefined), 'undefined', 'Expect undefined to be typeof "undefined".');
 		strictEqual(PURE.typeOf([1, 2, 3]), 'array', 'Expect array literal to be typeof "array".');
