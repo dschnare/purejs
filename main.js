715ccec4a500c4a8f65205bd422785855575b72f
Darren Schnare <darren@darrenschnare.com>
2012-06-28 21:37:53 -0400

    Add module name to source header

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /**
- * @preserve Author: Darren Schnare
+ * @preserve Module: purejs
+ * Author: Darren Schnare
  * Keywords: Keywords: javascript,constructor,inheritence,mixin,pure,type,testing
  * License: MIT ( http://www.opensource.org/licenses/mit-license.php )
  * Repo: https://github.com/dschnare/purejs
